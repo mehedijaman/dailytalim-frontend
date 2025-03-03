ef04b0179af9c8773bf95e8ad50c73ef9bec5a9f
sajid islam <sajidisalm941@gmail.com>
2025-03-03 14:06:47 +0600

    style(right-sidebar): width full on small device

--- src/components/rightSidebar/RightSidebar.jsx
+++ src/components/rightSidebar/RightSidebar.jsx
@@ -1,13 +1,15 @@
 'use client';
 
+import useWindowWidth from '@/hooks/useWindowWidth';
 import Date from './Date';
 import PrayerTimeTable from './PrayerTimeTable';
 import { IoMdClose } from 'react-icons/io';
 
 const RightSidebar = ({ isRightSidebarOpen, setIsRightSidebarOpen }) => {
+  const width = useWindowWidth();
   return (
     <div
-      className={`scrollbar-hide dark:border-dailyTalim-border-dark fixed top-16 h-screen space-y-5 overflow-y-auto border-l bg-white px-6 pb-6 pt-3 md:w-72 md:pt-0 ${isRightSidebarOpen ? 'right-0' : 'right-0 translate-x-full'} dark:bg-dailyTalim-darkBg transition-transform duration-300`}
+      className={`scrollbar-hide fixed top-16 h-screen space-y-5 overflow-y-auto border-l bg-white px-6 pb-6 pt-3 dark:border-dailyTalim-border-dark md:w-72 md:pt-0 ${isRightSidebarOpen ? 'right-0' : 'right-0 translate-x-full'} transition-transform duration-300 dark:bg-dailyTalim-darkBg ${width <= 442 && 'w-full'}`}
     >
       <div className="flex justify-end md:hidden">
         <button onClick={() => setIsRightSidebarOpen(false)}>
