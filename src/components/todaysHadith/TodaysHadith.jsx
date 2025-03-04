0c00b5caa2e91ec8a02948d32c585fe25934f4db
MASUMHD <mrmasum2k4@gmail.com>
2025-03-04 14:46:45 +0600

    add

--- src/components/todaysHadith/TodaysHadith.jsx
+++ src/components/todaysHadith/TodaysHadith.jsx
@@ -4,7 +4,7 @@ import HadithHeader from './HadithHeader';
 
 const TodaysHadith = () => {
   return (
-    <main className="">
+    <main>
       <HadithHeader />
       <div className="mx-auto w-full rounded-xl p-6 transition-all duration-300 md:p-8">
         <div className="rtl mt-5 text-right">
