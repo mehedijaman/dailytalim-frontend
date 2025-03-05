0266f328bfaf69d5bf1277d0cb75464586456948
sajid islam <sajidisalm941@gmail.com>
2025-03-05 15:50:42 +0600

    style(todays-hadith): hidden download button text on small device

--- src/components/todaysHadith/DownloadButton.jsx
+++ src/components/todaysHadith/DownloadButton.jsx
@@ -5,7 +5,7 @@ const DownloadButton = ({ handleDownloadHadithImage }) => {
   return (
     <div onClick={handleDownloadHadithImage} className="flex justify-end">
       <Button className="rounded-[6px]">
-        ডাউনলোড <FiDownload />{' '}
+        <span className="hidden md:block">ডাউনলোড</span> <FiDownload />{' '}
       </Button>
     </div>
   );
