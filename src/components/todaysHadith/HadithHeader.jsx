c5b1c92ea3de5aa7f2e1d9d01f8a9f28d54c82b6
sajid-islam <sajidislam941@gmail.com>
2025-03-05 15:45:13 +0600

    style(todays-hadith): fix alignment issue

--- src/components/todaysHadith/HadithHeader.jsx
+++ src/components/todaysHadith/HadithHeader.jsx
@@ -1,13 +1,13 @@
 import { LuCopy } from 'react-icons/lu';
-import { HiDotsHorizontal } from 'react-icons/hi';
-import { Button } from '../ui/button';
-import { SiKnexdotjs } from 'react-icons/si';
+import { FaStar } from 'react-icons/fa';
+import { CiStar } from 'react-icons/ci';
 import DownloadButton from './DownloadButton';
 
 const HadithHeader = ({ handleDownloadHadithImage }) => {
   return (
     <main className="mt-8 flex items-center justify-between">
-      <div className="text-xl text-green-600 md:gap-4 md:text-2xl">
+      <div className="flex items-center gap-2 text-xl text-green-600 md:text-2xl">
+        <CiStar size={30} />
         <h2 className="font-bold">আজকের তালিম</h2>
       </div>
 
