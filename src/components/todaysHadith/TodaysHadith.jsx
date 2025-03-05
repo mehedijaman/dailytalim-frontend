f36a887fcef72cc2f23f5d5125eac85fb21495b2
sajid islam <sajidisalm941@gmail.com>
2025-03-05 23:51:44 +0600

    Style(todays-hadith): change header and footer color

--- src/components/todaysHadith/TodaysHadith.jsx
+++ src/components/todaysHadith/TodaysHadith.jsx
@@ -21,11 +21,11 @@ const TodaysHadith = () => {
           {/* Hidden heading: This is diplayed on the hadith photo card */}
           <div
             id="hadith-header"
-            className="hidden rounded-t-md bg-dailyTalim-primary-500 py-2"
+            className="rounded-t-md bg-dailyTalim-darkBg px-5 pt-4 text-dailyTalim-primary-500"
           >
-            <div className="flex items-center justify-center gap-4">
+            <div className="flex items-center justify-center gap-4 border-b border-dailyTalim-primary-500 px-5 pb-2">
               <h2 className="text-2xl font-semibold">আজকের তালিম</h2>
-              <div className="border-l-2 pl-4">
+              <div className="border-l-2 border-dailyTalim-primary-500 pl-4">
                 <p className="font-semibold">{formattedHijriDateInBangla}</p>
                 <p className="flex gap-2">
                   <span>{formattedBanglaDate}</span> |{' '}
@@ -77,7 +77,7 @@ const TodaysHadith = () => {
 
             {/* Hidden footer: This is displayed on hadith photo card*/}
             <footer id="hadith-footer" className="hidden">
-              <div className="mt-4 flex items-center gap-3 border-t px-2 pb-1 pt-2 font-semibold">
+              <div className="mt-4 flex items-center gap-3 border-t border-dailyTalim-primary-500 px-2 pb-1 pt-2 font-semibold text-dailyTalim-primary-500">
                 <p className="flex items-center gap-2">
                   <IoBookOutline size={20} />
                   DailyTalim
