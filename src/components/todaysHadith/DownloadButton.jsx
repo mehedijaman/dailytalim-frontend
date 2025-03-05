4255718e4abfcb452bb8d7c87978112689ec48b8
sajid-islam <sajidislam941@gmail.com>
2025-03-05 18:53:39 +0600

    move hadith photo card downloder function DownloadButton component

--- src/components/todaysHadith/DownloadButton.jsx
+++ src/components/todaysHadith/DownloadButton.jsx
@@ -1,7 +1,34 @@
 import { Button } from '../ui/button';
 import { FiDownload } from 'react-icons/fi';
+import * as htmlToImage from 'html-to-image';
+import download from 'downloadjs';
 
-const DownloadButton = ({ handleDownloadHadithImage }) => {
+const DownloadButton = () => {
+  const handleDownloadHadithImage = () => {
+    const hadithElement = document.getElementById('hadith');
+    const footer = document.getElementById('hadith-footer');
+    const hadithHeader = document.getElementById('hadith-header');
+
+    footer.classList.remove('hidden');
+    hadithHeader.classList.remove('hidden');
+    htmlToImage
+      .toPng(hadithElement, {
+        style: {
+          borderRadius: '12px',
+          overflow: 'hidden',
+        },
+      })
+      .then(imgUrl => {
+        download(imgUrl, 'hadith.png');
+        footer.classList.add('hidden');
+        hadithHeader.classList.add('hidden');
+      })
+      .catch(error => {
+        console.error('Error generating image:', error);
+        footer.classList.add('hidden');
+        hadithHeader.classList.add('hidden');
+      });
+  };
   return (
     <div onClick={handleDownloadHadithImage} className="flex justify-end">
       <Button className="rounded-[6px]">
