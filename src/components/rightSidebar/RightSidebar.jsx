b111fec8e0cf920ca7d01bf13d6264822803f2a0
MASUMHD <mrmasum2k4@gmail.com>
2025-03-07 00:38:05 +0600

    add custom hook to font size

--- src/components/rightSidebar/RightSidebar.jsx
+++ src/components/rightSidebar/RightSidebar.jsx
@@ -6,11 +6,12 @@ import PrayerTimeTable from './PrayerTimeTable';
 import { IoMdClose } from 'react-icons/io';
 import FontSize from '../todaysHadith/FontSize';
 import { useState } from 'react';
+import useFontSize from '@/hooks/useFontSize';
 
 const RightSidebar = ({ isRightSidebarOpen, setIsRightSidebarOpen }) => {
   const width = useWindowWidth();
 
-  const [fontSize, setFontSize] = useState({ arabic: 24, bangla: 19 });
+  const { fontSize, setFontSize } = useFontSize();
   return (
     <div
       className={`scrollbar-hide fixed top-16 h-screen space-y-5 overflow-y-auto border-l bg-white px-6 pb-6 pt-3 dark:border-dailyTalim-border-dark md:w-72 md:pt-0 ${isRightSidebarOpen ? 'right-0' : 'right-0 translate-x-full'} transition-transform duration-300 dark:bg-dailyTalim-darkBg ${width <= 442 && 'w-full'}`}
@@ -21,7 +22,7 @@ const RightSidebar = ({ isRightSidebarOpen, setIsRightSidebarOpen }) => {
         </button>
       </div>
       <Date />
-      <FontSize  fontSize={fontSize} setFontSize={setFontSize} />
+      <FontSize fontSize={fontSize} setFontSize={setFontSize} />
       <section>
         <PrayerTimeTable />
       </section>
