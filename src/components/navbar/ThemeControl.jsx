d7fde9dda088c74d69284ecb8c8c5be3b88474d8
sajid-islam <sajidislam941@gmail.com>
2025-03-07 22:47:24 +0600

    Fix: hydration wraning on themeControl

--- src/components/navbar/ThemeControl.jsx
+++ src/components/navbar/ThemeControl.jsx
@@ -3,12 +3,21 @@
 import { IoMdSunny } from 'react-icons/io';
 import { IoMoon } from 'react-icons/io5';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 const ThemeControl = () => {
   const { theme, setTheme } = useTheme();
 
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
-    <div suppressHydrationWarning>
+    <div>
       <div
         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
         className="cursor-pointer rounded-full bg-dailyTalim-primary-100 p-2 dark:bg-dailyTalim-border-dark"
