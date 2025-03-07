f66296bc272bedb15aee3df533ddd5781572a88f
MASUMHD <mrmasum2k4@gmail.com>
2025-03-07 15:38:36 +0600

    add contact page

--- src/hooks/useFontSize.jsx
+++ src/hooks/useFontSize.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 const useFontSize = () => {
+    
   const [fontSize, setFontSize] = useState({ arabic: 24, bangla: 19 });
 
   return { fontSize, setFontSize };
