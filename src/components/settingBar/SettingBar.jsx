23baa2f8288fded829dc5f36ffb811bda8e32260
sajid-islam <sajidislam941@gmail.com>
2025-03-08 22:31:04 +0600

    Add setting title on setting bar

--- src/components/settingBar/SettingBar.jsx
+++ src/components/settingBar/SettingBar.jsx
@@ -9,6 +9,8 @@ const SettingBar = () => {
     <div
       className={`fixed top-16 h-screen w-72 border-l bg-white p-6 dark:border-dailyTalim-border-dark dark:bg-dailyTalim-darkBg ${isSettingBarOpen ? 'right-0' : 'right-0 translate-x-full'} transition-transform duration-300`}
     >
+      <h2 className="mb-4 text-lg font-semibold">Settings</h2>
+
       <FontResize />
     </div>
   );
