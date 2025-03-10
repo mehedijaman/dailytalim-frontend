648826a37d3b33b8ca260470cc923993c2a9b6fd
sajid-islam <sajidislam941@gmail.com>
2025-03-10 16:54:16 +0600

    feat(theme): Impelement warm theme and fix: footer width problem

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,4 +1,5 @@
 'use client';
+import Footer from '@/components/Footer/Footer';
 import Navbar from '@/components/navbar/Navbar';
 import NavBarBottom from '@/components/navBarBottom/NavBarBottom';
 import RightSidebar from '@/components/rightSidebar/RightSidebar';
@@ -10,17 +11,20 @@ const MainLayout = ({ children }) => {
 
   return (
     <main>
-      <header className="border-border-color fixed z-40 h-16 w-full border-b bg-background lg:z-50">
+      <header className="fixed z-40 h-16 w-full border-b border-border-color bg-background lg:z-50">
         <Navbar />
       </header>
-      <section className="flex gap-4">
+      <section className="flex">
         <aside className="z-50 lg:z-auto lg:pt-16">
           <Sidebar />
         </aside>
         <div
           className={`flex-1 ${isSidebarOpen ? 'lg:pl-72' : 'pl-0'} transition-padding py-16 duration-300 md:pr-72`}
         >
-          <div className="mx-auto my-8 max-w-4xl">{children}</div>
+          <div>
+            <div className="mx-auto my-8 max-w-6xl px-4">{children}</div>
+            <Footer />
+          </div>
         </div>
         <aside className="z-40 md:pt-16">
           <RightSidebar />
