670e723b7066532dc7d9355921964128437e6a44
sajid islam <sajidisalm941@gmail.com>
2025-03-13 15:24:18 +0600

    fetch hadiths from api and show chpates on sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,24 @@
 import useSidebarsContext from '@/hooks/useSidebarsContext';
 import Search from '@/components/Search/Search';
+import useAxiosPublic from '@/hooks/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
 
 const Sidebar = () => {
   const { isSidebarOpen, setIsSidebarOpen } = useSidebarsContext();
+  const axiosPublic = useAxiosPublic();
+  const convertToBanglaDigits = num => {
+    const banglaDigit = ['০', '১', '২', '৩', '৪', '৫', '৬', '৭', '৮', '৯'];
+    return num.toString().replace(/\d/g, digit => banglaDigit[digit]);
+  };
+
+  const { data: chapters = [] } = useQuery({
+    queryKey: ['hadiths'],
+    queryFn: async () => {
+      const hadithRes = await axiosPublic.get('/hadiths');
+      return hadithRes.data.flatMap(hadith => hadith.chapters);
+    },
+  });
+  console.log(chapters);
   return (
     <div
       className={`${!isSidebarOpen && 'pointer-events-none'} fixed flex h-screen`}
@@ -17,18 +33,23 @@ const Sidebar = () => {
         aria-hidden={!isSidebarOpen}
       />
       <div
-        className={`border-border-color bg-sidebar-bg h-screen w-72 overflow-y-auto border-r ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} z-50 transition-all duration-300 ease-in-out`}
+        className={`h-screen w-72 overflow-y-auto border-r border-border-color bg-sidebar-bg ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} z-50 transition-all duration-300 ease-in-out`}
       >
-        <div className="p-6">
+        <div className="p-6 pb-24">
           <Search />
-          <div className="bg-secondary-1 cursor-pointer rounded-md px-4 py-2 mt-5">
-            <h5 className="font-semibold">সহিহ বুখারি</h5>
-            <p className="text-sm text-muted">
-              মোট হাদিস <span>৭৫৪৪</span>
-            </p>
-          </div>
+          {chapters.map((chapter, i) => (
+            <div
+              key={i}
+              className="mt-5 cursor-pointer rounded-md bg-secondary-1 px-4 py-2"
+            >
+              <h5 className="font-semibold">{chapter.name}</h5>
+              <p className="text-sm text-muted">
+                মোট হাদিস{' '}
+                <span>{convertToBanglaDigits(chapter?.hadiths?.length)}</span>
+              </p>
+            </div>
+          ))}
         </div>
-        
       </div>
     </div>
   );
