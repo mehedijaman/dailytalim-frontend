8b80ef1cb238935e04aed6c012adaa2cba89b30b
MASUMHD <mrmasum2k4@gmail.com>
2025-03-14 22:59:51 +0600

    add to page blog and about

---
+++ src/app/(main)/about/page.jsx
@@ -0,0 +1,12 @@
+
+const page = () => {
+    return (
+        <div>
+            <div className="flex items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold">About is work runing now ... </h1>
+            </div>
+        </div>
+    );
+};
+
+export default page;
