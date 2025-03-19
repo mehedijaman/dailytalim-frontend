2df0e302e35cfc297e40ad9944720e3bb705c4a0
sajid islam <sajidisalm941@gmail.com>
2025-03-19 19:45:25 +0600

    feat(sidebar): Implement real-time search for sidebar menus using fuse.js

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,16 +1,18 @@
-import { FaSearch } from "react-icons/fa";
-import { Input } from "@/components/ui/input";
+import { FaSearch } from 'react-icons/fa';
+import { Input } from '@/components/ui/input';
 
-const Search = () => {
-    return (
-        <div className="flex items-center  border border-gray-500 rounded-lg px-4 py-1  ">
-            <FaSearch size={20} className="text-gray-600" />
-            <Input 
-                placeholder="অনুসন্ধান করুন..." 
-                className="outline-none border-none bg-transparent placeholder-gray-500  text-gray-700 w-full"
-            />
-        </div>
-    );
+const Search = ({ searchQuery, setSearchQuery }) => {
+  return (
+    <div className="flex items-center rounded-lg border border-gray-500 px-4 py-1">
+      <FaSearch size={20} className="text-gray-600" />
+      <Input
+        placeholder="অনুসন্ধান করুন..."
+        className="w-full border-none bg-transparent text-gray-700 placeholder-gray-500 outline-none"
+        value={searchQuery}
+        onChange={e => setSearchQuery(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Search;
