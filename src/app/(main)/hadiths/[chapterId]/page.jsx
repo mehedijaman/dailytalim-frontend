7265dd622db0d31885cd0c3f710e23fd655a5d43
sajid-islam <sajidislam941@gmail.com>
2025-03-19 17:43:57 +0600

    Add: No hadith message

--- src/app/(main)/hadiths/[chapterId]/page.jsx
+++ src/app/(main)/hadiths/[chapterId]/page.jsx
@@ -23,28 +23,38 @@ const HadithsPage = ({ params }) => {
 
   return (
     <div className="space-y-10 px-5">
-      {chapter?.hadiths?.map(hadith => (
-        <div key={hadith.id}>
-          <div id="hadith" className="w-full border-b-2 pb-5">
-            {/*Arabic Hadith */}
-            <div className="text-right">
-              <p
-                className="mt-2 cursor-pointer text-2xl leading-relaxed"
-                style={{ fontSize: `${arabicFontSize}px` }}
-                dangerouslySetInnerHTML={{ __html: hadith?.ar }}
-              />
-            </div>
-            {/* Hadith Bangla Translation*/}
-            <div className="mt-5">
-              <p
-                className="mt-2 cursor-pointer text-left text-lg leading-relaxed"
-                style={{ fontSize: `${banglaFontSize}px` }}
-                dangerouslySetInnerHTML={{ __html: hadith?.bn }}
-              />
+      {chapter?.hadiths?.length > 0 ? (
+        chapter?.hadiths?.map(hadith => (
+          <div key={hadith.id}>
+            <div id="hadith" className="w-full border-b-2 pb-5">
+              {/*Arabic Hadith */}
+              <div className="text-right">
+                <p
+                  className="mt-2 cursor-pointer text-2xl leading-relaxed"
+                  style={{ fontSize: `${arabicFontSize}px` }}
+                  dangerouslySetInnerHTML={{ __html: hadith?.ar }}
+                />
+              </div>
+              {/* Hadith Bangla Translation*/}
+              <div className="mt-5">
+                <p
+                  className="mt-2 cursor-pointer text-left text-lg leading-relaxed"
+                  style={{ fontSize: `${banglaFontSize}px` }}
+                  dangerouslySetInnerHTML={{ __html: hadith?.bn }}
+                />
+              </div>
             </div>
           </div>
+        ))
+      ) : (
+        <div className="flex h-[calc(100vh-220px)] flex-col items-center justify-center">
+          <h4 className="text-xl font-bold"> দুঃখিত!</h4>
+          <p className="text-muted">এই বিষয়ে বর্তমানে কোনো হাদিস নেই।</p>
+          <p className="text-muted">
+            ইনশাআল্লাহ, খুব শীঘ্রই নতুন হাদিস যোগ করা হবে।
+          </p>
         </div>
-      ))}
+      )}
     </div>
   );
 };
