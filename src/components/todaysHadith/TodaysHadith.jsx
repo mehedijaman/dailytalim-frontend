eee39f1660f7b09bb3a15d3f1b24f79ced5a0b71
sajid-islam <sajidislam941@gmail.com>
2025-03-21 11:51:49 +0600

    style: set hadiths  text justify

--- src/components/todaysHadith/TodaysHadith.jsx
+++ src/components/todaysHadith/TodaysHadith.jsx
@@ -7,7 +7,6 @@ import useAllDate from '@/hooks/useAllDate';
 import useFontResize from '@/hooks/useFontResize';
 import { useQuery } from '@tanstack/react-query';
 import useAxiosPublic from '@/hooks/useAxiosPublic';
-import { useEffect, useState } from 'react';
 import HadithLoader from '@/app/(main)/hadiths/loading';
 
 const {
@@ -60,7 +59,7 @@ const TodaysHadith = () => {
             {/*Arabic Hadith */}
             <div className="text-right">
               <p
-                className="mt-2 cursor-pointer text-2xl leading-relaxed"
+                className="mt-2 cursor-pointer text-justify text-2xl leading-relaxed"
                 style={{ fontSize: `${arabicFontSize}px` }}
                 dangerouslySetInnerHTML={{
                   __html: todaysHadith?.todaysHadith?.ar,
@@ -70,7 +69,7 @@ const TodaysHadith = () => {
             {/* Hadith Bangla Translation*/}
             <div className="mt-5">
               <p
-                className="mt-2 cursor-pointer text-left text-lg leading-relaxed"
+                className="mt-2 cursor-pointer text-justify text-lg leading-relaxed"
                 style={{ fontSize: `${banglaFontSize}px` }}
                 dangerouslySetInnerHTML={{
                   __html: todaysHadith?.todaysHadith?.bn,
