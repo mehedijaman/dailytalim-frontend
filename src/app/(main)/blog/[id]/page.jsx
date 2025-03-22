95841f4ae2116830814bfcbed6a2bd262843f589
sajid islam <sajidisalm941@gmail.com>
2025-03-22 07:18:24 +0600

    Fix(blog): blog card color theme issue, Responsive issue and date issue

--- src/app/(main)/blog/[id]/page.jsx
+++ src/app/(main)/blog/[id]/page.jsx
@@ -30,28 +30,24 @@ const BlogDetails = () => {
     <div className="container mx-auto py-8">
       <Link href="/blog">
         <div className="fixed flex items-center">
-          <p className="hover:cursor-pointer hover:text-[#0bad4c]">
+          <p className="hover:cursor-pointer">
             <IoArrowBackSharp size={30} />
           </p>
         </div>
       </Link>
       <div className="mb-6 flex items-center justify-center">
-        <div className="mt-auto flex items-center rounded bg-[#0bad4c] px-4 py-1 text-white">
+        <div className="mt-auto flex items-center rounded bg-primary-500 px-4 py-1 text-white">
           <p className="text-sm font-semibold">{blog.category}</p>
         </div>
       </div>
       <h1 className="mb-4 text-center text-4xl font-bold">{blog.title}</h1>
       <p className="text-center text-sm">
-        <span className="hover:cursor-pointer hover:text-[#0bad4c]">
-          {blog.reference}
-        </span>{' '}
+        <span className="hover:cursor-pointer">{blog.reference}</span>{' '}
         <span className="pl-2 pr-2">/</span>{' '}
-        <span className="hover:cursor-pointer hover:text-[#0bad4c]">
-          {blog.date}
-        </span>
+        <span className="hover:cursor-pointer">{blog.date}</span>
       </p>
       <hr className="my-10" />
-      <div className="mx-auto max-w-3xl">
+      <div className="mx-auto text-justify font-noto-serif-banglai md:px-5">
         <p className="text-lg leading-relaxed">{blog.ShortHadith}</p>
         <p className="mt-6 text-lg">{blog.longHadith}</p>
       </div>
