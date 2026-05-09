cecc4c77f6bc09a6d859e69f3973dc702e6e6acc
sajid-islam <sajidislam941@gmail.com>
2026-05-09 20:21:00 +0600

    chore: upgrade next.js 15.1.11 to 16.2.6

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -10,12 +11,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends(
-    "next/core-web-vitals",
-    "plugin:import/recommended",
-    "plugin:playwright/recommended",
-    "plugin:prettier/recommended",
-  ),
+  ...nextCoreWebVitals,
+  ...compat.extends("plugin:import/recommended"),
+  ...compat.extends("plugin:playwright/recommended"),
+  ...compat.extends("plugin:prettier/recommended"),
   {
     plugins: ["simple-import-sort", "no-unused-imports"],
     rules: {
@@ -33,6 +32,9 @@ const eslintConfig = [
       },
     ],
   },
+  {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+  }
 ];
 
 export default eslintConfig;
